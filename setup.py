eb361e9bee25287a94d245b89820a3c256e94137
noah <noah.bouma@gmail.com>
2018-10-14 16:06:49 -0600

    Requirements in requirements.txt

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import find_packages
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
 setup(
     author="OnePair",
     name="authid-agent-client",
     version="0.0.1",
     packages=find_packages(),
+    install_requires=required,
     entry_points={
         "console_scripts": [
             "authid-cli = authid_agent_client.cli.agent_client_cli:run_cli"
